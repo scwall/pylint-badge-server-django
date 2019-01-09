3ceffa82aed8d81fa73cc44a20d3175fcb4cf2c6
scwall <deselys.pascal@gmail.com>
2019-01-09 14:46:06 +0100

    add create pylint_badge function, modify view for create badge

--- pylint/urls.py
+++ pylint/urls.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 from django.urls import path
 
-from pylint.views import main_page
+from pylint.views import main_page, reports
 
 app_name = 'pylint'
 urlpatterns = [
     path('', main_page,name='main_page'),
-
+    path('reports',reports,name='reports'),
 ]
--- pylint/views.py
+++ pylint/views.py
@@ -1,16 +1,22 @@
 import jwt
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 from pylint_badge_server.settings import SECRET_KEY
+from pylint.pylint_badge import get_rating_and_colour
+import json
+
 # Create your views here.
 def main_page(request):
 
     return render(request,template_name='index.html')
-
-def report(request):
+@csrf_exempt
+def reports(request):
     if request.method == 'POST':
         travis_job_id = request.POST['travis_job_id']
-        pylint_report = request.FILES
+        pylint_report = request.FILES['pylint-report'].read().decode('utf-8')
         pylint_token = jwt.decode(request.POST['pylint_badge_token'], SECRET_KEY, algorithms=['HS256'])
-        print(pylint_token)
+        pylint_badge = get_rating_and_colour()
+        return HttpResponse('Ok', status=200)
+
     return HttpResponse('bad request', status=400)
