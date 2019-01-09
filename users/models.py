3fc3436f9fb7b21809d6c1289d6700b1804f5ce7
scwall <deselys.pascal@gmail.com>
2019-01-09 15:33:17 +0100

    add new models user, modify index.html

--- users/models.py
+++ users/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    """
+    Model for the user
+    """
+    name = models.CharField()
+
+
+class Repository(models.Model):
+    name = models.CharField()
+    token = models.CharField()
+
+
+class Error(models.Model):
+    error = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Warning(models.Model):
+    warning = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Refactor(models.Model):
+    refactor = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Convention(models.Model):
+    convention = models.ForeignKey(User, on_delete=models.CASCADE)
