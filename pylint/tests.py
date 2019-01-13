61afac53405c574c95f59899bd54264848b5c5a2
scwall <deselys.pascal@gmail.com>
2019-01-13 14:42:36 +0100

    add test for colour

--- pylint/tests.py
+++ pylint/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from unittest.mock import patch, MagicMock
 
+from django.test import TestCase
+from pylint.pylint_badge import get_rating_and_colour
 # Create your tests here.
+
+class PylintBadgeServer(TestCase):
+    maxDiff = None
+    """
+    Test rating_and_colour
+    """
+    @patch('pylint.pylint_badge.BADGE_TEMPLATE', '{1} {0:.2f}')
+    def test_colour_report(self):
+        report = "Your code has been rated at 6/10"
+        colour_report = get_rating_and_colour(report)
+        self.assertEqual(colour_report, 'b94947 6.00')
+
